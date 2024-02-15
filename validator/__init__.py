5672147182180bdc24d2402187f5fa6982daeccd
karan@guardrailsai.com <thekaranacharya@gmail.com>
2024-02-15 15:51:52 -0500

    Update

--- validator/__init__.py
+++ validator/__init__.py
@@ -1,3 +1,3 @@
-from .main import RegexMatch
+from .main import SecretsPresent
 
-__all__ = ["RegexMatch"]
+__all__ = ["SecretsPresent"]
--- validator/main.py
+++ validator/main.py
@@ -1,8 +1,6 @@
-import re
-import string
-from typing import Any, Callable, Dict, Optional
-
-import rstr
+import os
+import warnings
+from typing import Any, Callable, Dict, List, Tuple, Union
 
 from guardrails.validator_base import (
     FailResult,
@@ -13,59 +11,176 @@ from guardrails.validator_base import (
 )
 
 
-@register_validator(name="guardrails/regex_match", data_type="string")
-class RegexMatch(Validator):
-    """Validates that a value matches a regular expression.
+@register_validator(name="guardrails/secrets_present", data_type="string")
+class SecretsPresent(Validator):
+    """Validates whether the generated code snippet contains any secrets.
 
     **Key Properties**
-
     | Property                      | Description                       |
     | ----------------------------- | --------------------------------- |
-    | Name for `format` attribute   | `regex_match`                     |
+    | Name for `format` attribute   | `guardrails/secrets_present`      |
     | Supported data types          | `string`                          |
-    | Programmatic fix              | Generate a string that matches the regular expression |
+    | Programmatic fix              | None                              |
 
     Args:
-        regex: Str regex pattern
-        match_type: Str in {"search", "fullmatch"} for a regex search or full-match option
-    """  # noqa
+        None
 
-    def __init__(
-        self,
-        regex: str,
-        match_type: Optional[str] = None,
-        on_fail: Optional[Callable] = None,
-    ):
-        # todo -> something forces this to be passed as kwargs and therefore xml-ized.
-        # match_types = ["fullmatch", "search"]
+    This validator uses the `detect-secrets` library to check whether the generated code
+    snippet contains any secrets. If any secrets are detected, the validator fails and
+    returns the generated code snippet with the secrets replaced with asterisks.
+    Else the validator returns the generated code snippet.
 
-        if match_type is None:
-            match_type = "fullmatch"
-        assert match_type in [
-            "fullmatch",
-            "search",
-        ], 'match_type must be in ["fullmatch", "search"]'
+    Following are some caveats:
+        - Multiple secrets on the same line may not be caught. e.g.
+            - Minified code
+            - One-line lists/dictionaries
+            - Multi-variable assignments
+        - Multi-line secrets may not be caught. e.g.
+            - RSA/SSH keys
 
-        super().__init__(on_fail=on_fail, match_type=match_type, regex=regex)
-        self._regex = regex
-        self._match_type = match_type
+    Example:
+        ```py
 
-    def validate(self, value: Any, metadata: Dict) -> ValidationResult:
-        p = re.compile(self._regex)
-        """Validates that value matches the provided regular expression."""
-        # Pad matching string on either side for fix
-        # example if we are performing a regex search
-        str_padding = (
-            "" if self._match_type == "fullmatch" else rstr.rstr(string.ascii_lowercase)
+        guard = Guard.from_string(validators=[
+            DetectSecrets(on_fail="fix")
+        ])
+        guard.parse(
+            llm_output=code_snippet,
         )
-        self._fix_str = str_padding + rstr.xeger(self._regex) + str_padding
+        ```
+    """
 
-        if not getattr(p, self._match_type)(value):
+    def __init__(self, on_fail: Union[Callable[..., Any], None] = None, **kwargs):
+        super().__init__(on_fail, **kwargs)
+
+        self.temp_file_name = "temp.txt"
+        self.mask = "********"
+
+    def get_unique_secrets(self, value: str) -> Tuple[Dict[str, Any], List[str]]:
+        """Get unique secrets from the value.
+
+        Args:
+            value (str): The generated code snippet.
+
+        Returns:
+            unique_secrets (Dict[str, Any]): A dictionary of unique secrets and their
+                line numbers.
+            lines (List[str]): The lines of the generated code snippet.
+        """
+        try:
+            # Write each line of value to a new file
+            with open(self.temp_file_name, "w") as f:
+                f.writelines(value)
+        except Exception as e:
+            raise OSError(
+                "Problems creating or deleting the temporary file. "
+                "Please check the permissions of the current directory."
+            ) from e
+
+        try:
+            # Create a new secrets collection
+            from detect_secrets import settings
+            from detect_secrets.core.secrets_collection import SecretsCollection
+
+            secrets = SecretsCollection()
+
+            # Scan the file for secrets
+            with settings.default_settings():
+                secrets.scan_file(self.temp_file_name)
+        except ImportError:
+            raise ValueError(
+                "You must install detect-secrets in order to "
+                "use the DetectSecrets validator."
+            )
+        except Exception as e:
+            raise RuntimeError(
+                "Problems with creating a SecretsCollection or "
+                "scanning the file for secrets."
+            ) from e
+
+        # Get unique secrets from these secrets
+        unique_secrets = {}
+        for secret in secrets:
+            _, potential_secret = secret
+            actual_secret = potential_secret.secret_value
+            line_number = potential_secret.line_number
+            if actual_secret not in unique_secrets:
+                unique_secrets[actual_secret] = [line_number]
+            else:
+                # if secret already exists, avoid duplicate line numbers
+                if line_number not in unique_secrets[actual_secret]:
+                    unique_secrets[actual_secret].append(line_number)
+
+        try:
+            # File no longer needed, read the lines from the file
+            with open(self.temp_file_name, "r") as f:
+                lines = f.readlines()
+        except Exception as e:
+            raise OSError(
+                "Problems reading the temporary file. "
+                "Please check the permissions of the current directory."
+            ) from e
+
+        try:
+            # Delete the file
+            os.remove(self.temp_file_name)
+        except Exception as e:
+            raise OSError(
+                "Problems deleting the temporary file. "
+                "Please check the permissions of the current directory."
+            ) from e
+        return unique_secrets, lines
+
+    def get_modified_value(
+        self, unique_secrets: Dict[str, Any], lines: List[str]
+    ) -> str:
+        """Replace the secrets on the lines with asterisks.
+
+        Args:
+            unique_secrets (Dict[str, Any]): A dictionary of unique secrets and their
+                line numbers.
+            lines (List[str]): The lines of the generated code snippet.
+
+        Returns:
+            modified_value (str): The generated code snippet with secrets replaced with
+                asterisks.
+        """
+        # Replace the secrets on the lines with asterisks
+        for secret, line_numbers in unique_secrets.items():
+            for line_number in line_numbers:
+                lines[line_number - 1] = lines[line_number - 1].replace(
+                    secret, self.mask
+                )
+
+        # Convert lines to a multiline string
+        modified_value = "".join(lines)
+        return modified_value
+
+    def validate(self, value: str, metadata: Dict[str, Any]) -> ValidationResult:
+        # Check if value is a multiline string
+        if "\n" not in value:
+            # Raise warning if value is not a multiline string
+            warnings.warn(
+                "The DetectSecrets validator works best with "
+                "multiline code snippets. "
+                "Refer validator docs for more details."
+            )
+
+            # Add a newline to value
+            value += "\n"
+
+        # Get unique secrets from the value
+        unique_secrets, lines = self.get_unique_secrets(value)
+
+        if unique_secrets:
+            # Replace the secrets on the lines with asterisks
+            modified_value = self.get_modified_value(unique_secrets, lines)
+
             return FailResult(
-                error_message=f"Result must match {self._regex}",
-                fix_value=self._fix_str,
+                error_message=(
+                    "The following secrets were detected in your response:\n"
+                    + "\n".join(unique_secrets.keys())
+                ),
+                fix_value=modified_value,
             )
         return PassResult()
-
-    def to_prompt(self, with_keywords: bool = True) -> str:
-        return "results should match " + self._regex
